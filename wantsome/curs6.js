d8b5bda9520c5c7d962aade20fc12d6f4abde2e3
fmirunamaria <focsacom93@yahoo.com>
2019-06-22 09:30:25 +0300

    mmm

---
+++ wantsome/curs6.js
@@ -0,0 +1,49 @@
+/* var age=18; */
+
+/* if (age<15) {
+    alert("25% reducere")
+} else if (age<15 && age<25) {
+    alert ("10% reducere")
+} else {
+    alert ("fara reducere")
+};
+*/
+
+/* switch (age) {
+    case 15:
+        alert("25% reducere");
+        break;
+    case 16:
+        alert("20% reducere");
+        break;
+    case 17:
+        alert("15% reducere");
+        break;
+    case 18:
+        alert("10% reducere");
+        break;
+    default:
+        alert("fara reducere");
+}; */
+
+/*if (age==15) {
+    alert ("25% reducere")
+} else if (age==16) {
+    alert ("20% reducere")
+} else if (age==17) {
+    alert ("15% reducere")
+} else if (age==18) {
+    alert ("10% reducere")
+} else {
+    alert ("fara reducere")
+}; */
+
+/* for (  var i=0; i<100; i+2 ) { 
+    console.log(j)
+}; */
+
+let j = 0;
+while (j < 100) {
+console.log(j);
+j++;
+}
